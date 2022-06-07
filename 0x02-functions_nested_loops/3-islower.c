dbf7c25fa57ef424c0c80dbb6321384640bc4a7a
Jason <emelieobumse100@gmail.com>
2022-06-07 16:57:06 -0700

    checking for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+include "main.h"
+
+/**
+ * _islower - check for lower characters
+ * returns 1 if alphabet is in lowecase
+ * returns 0 otherwise
+ */
+int _islower(int c)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c = 'a' && <= 'z')
+		{
+			return (1);
+		}
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,3 +5,7 @@ void print_alphabet_x10(void);
 /**
  * print_alphabet_x10 - print lowecase a-z
  */
+int _islower(int c);
+/**
+ * int _islower(int c) - checks for lcase alphabets
+ */

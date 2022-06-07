c3efe6d59580eac1bf558dff1fe853c5d48cb52f
Jason <emelieobumse100@gmail.com>
2022-06-07 15:53:07 -0700

    printing lcase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,12 +11,8 @@ void print_alphabet(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		putchar(ch);
+		_putchar(ch);
 	}
-}
-int main(void)
-{
-	print_alphabet();
-	putchar('\n');
+	_putchar ('\n');
 	return (0);
 }

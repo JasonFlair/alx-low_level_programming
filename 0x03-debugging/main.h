1ade55cacfe6acf5688cd7e55170ba45382bd357
Jason <emelieobumse100@gmail.com>
2022-06-08 17:59:29 -0700

    largest num

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.H>
+#include <stdio.h>
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 /* function to find the largest int */
 
-#endif //MAIN_H
+#endif /* MAIN_H */

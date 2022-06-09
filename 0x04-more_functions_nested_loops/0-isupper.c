11b53f36e5e64ae868f5bf3365cfdbc4c33a9a33
Jason <emelieobumse100@gmail.com>
2022-06-09 13:53:14 -0700

    checking for uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,11 @@
 /**
  * _isupper - check for uppercase characters
  * @c: is going to be checked
- * returns 1 if alphabet is in lowecase
- * returns 0 otherwise
+ * Returns: 1 if alphabet is in uppercase
+ * Returns: 0 otherwise
  */
 int _isupper(int c);
 {
-
-	int c;
-
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else

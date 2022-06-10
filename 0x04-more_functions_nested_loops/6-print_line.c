4553debb510f25e888aa3f5c708a7c5b7b75165c
Jason <emelieobumse100@gmail.com>
2022-06-09 18:02:30 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,7 +9,7 @@ void print_line(int n)
 {
 	if (n <= 0)
 	{
-		_putchar("\n");
+		_putchar('\n');
 	}
 	else
 	{
@@ -17,7 +17,7 @@ void print_line(int n)
 
 		for (i = 1; i <= n; i++)
 		{
-			_putchar('_');
+			_putchar("_");
 		}
 	}
 	_putchar('\n');

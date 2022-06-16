2b1ad33c79c94431b27867e5ce9a3bb3f80e6f4d
Jason <emelieobumse100@gmail.com>
2022-06-16 16:15:13 -0700

    lcase to ucase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,9 +12,9 @@ char *string_toupper(char *s)
 
 	while (*s != '\0')
 	{
-		if (s[i] >= 97 && s[i] <= 122)
+		if (*s[i] >= 97 && *s[i] <= 122)
 			/* i used the ascii values of lowercase alphabets */
-		s[i] = s[i] - 32;
+		*s[i] = *s[i] - 32;
 		i++;
 	}
 	return (s);

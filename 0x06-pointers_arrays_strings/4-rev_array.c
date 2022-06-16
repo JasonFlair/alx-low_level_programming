14d3a54e705af2ff718257aabafd36c31ce29f15
Jason <emelieobumse100@gmail.com>
2022-06-16 11:35:39 -0700

    rev arr

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,7 +12,7 @@ void reverse_array(int *a, int n)
 
 	for (int low = 0; high = n - 1; low < high; low++, high--)
 	{
-		int temp = arr[low];
+		temp = arr[low];
 		arr[low] = arr[high];
 		arr[high] = temp;
 	}

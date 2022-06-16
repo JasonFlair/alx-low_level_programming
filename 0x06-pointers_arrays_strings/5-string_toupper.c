77a830b122dd66e82c8056432b155f95a59739dd
Jason <emelieobumse100@gmail.com>
2022-06-16 13:06:00 -0700

    lcase to ucase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that changes all lowercase letters of a string to uppercase
+ * @s: input string
+ * Return: char
+ */
+char *string_toupper(char *s)
+{
+	while (*s != '\0')
+	{
+		if (*s >= 97 && *s <= 122) /* using the ascii values of lowercase alphabets */
+			*s = *s - 32;
+	}
+	return(s);
+}

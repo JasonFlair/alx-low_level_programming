3ff0f6451e5366ef676cebb6a1b24b590f24b56f
Jason <emelieobumse100@gmail.com>
2022-06-22 21:37:11 -0700

    raised to power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * factorial - returns the factorial of a given number
+ * _pow_recursion - returns the value of x raised to the power of y
  * @x: base
  * @y: exponent
  * Return: the value/answer

300704a965ce06b01556e7aac0ca5ae1b77d5ac4
Jason <emelieobumse100@gmail.com>
2022-06-22 21:03:23 -0700

    string length recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,13 +9,6 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (*s == '\0')
-	{
-		return (0);
-	}
-	while (s[i] != '0')
-	{
-		i++;
-	}
-	return (i);
+	if (*s != '\0')
+		return (1 + _strlen_recursion(++s));
 }

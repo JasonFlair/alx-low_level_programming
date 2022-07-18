2b31421cd04e9fcb366b7a7f65cc61010083372b
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-07-18 17:17:26 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,12 +9,12 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t num = 0;
 
 	while (h != NULL)
 	{
 		h = h->next;
-		nnodes++;
+		num++;
 	}
-	return (nnodes);
+	return (num);
 }

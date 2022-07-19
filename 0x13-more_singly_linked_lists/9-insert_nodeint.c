e45bd91e8a2c9ae89ff53ac73a1af3dbf46d7d12
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-07-19 02:44:54 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
     }
     else
     {
+        if (h == NULL && idx != 0)
+		return (NULL);
         for (i = 0; i < idx - 1; i++)
         {
             ptr = ptr->next;

2e88e7752d0b04721bd1c9869fd7b0ea5d65bda4
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-07-21 20:37:15 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,21 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: input stringeturn
- * Return: length of a string
- */
-int _strlen(char *s)
-{
-	int count = 0;
-
-	while (s[count] != '\0')
-	{
-		count++;
-	}
-
-	return (count);
-}
-
-/**
  * binary_to_uint - converts a binary number to an
  * unsigned int.
  * @b: binary.
  *
  * Return: unsigned int.
  */
-unsigned int binary_to_uint( const char *b)
+unsigned int binary_to_uint(const char *b)
 {
- unsigned int base_two = 1;
+ unsigned int decval = 1;
  unsigned int ui=0;
  int c;
  unsigned int len;
 
- len = _strlen(b);
+ for (len = 0; b[len] != '\0'; len++)
+ ;
 
   for (c = len-1; c >= 0; c--)
  {
@@ -39,9 +23,9 @@ unsigned int binary_to_uint( const char *b)
       return (0);
     if (b[c] == '1')
     {
-     ui += base_two;
+     ui += decval;
      }
-    base_two *= 2;
+    decval *= 2;
 }
  return (ui);
 }

20d494469b7e473e3093ba7878c71accca928614
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-07-25 18:40:25 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,14 @@
-ssize_t read_textfile(const char *filename, size_t letters)
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+ 
+ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t fileread, filewritten;

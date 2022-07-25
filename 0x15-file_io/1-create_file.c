3b70afe1a2db724af4e2d4fa09851d71325a7cec
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-07-25 21:44:26 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,8 +30,6 @@ int create_file(const char *filename, char *text_content)
 		;
 	}
 	writtenFile = write(fd, text_content, numOfLetters);
-	if (writtenFile == -1)
-		return (-1);
 
 	close(fd);
 	return 1;

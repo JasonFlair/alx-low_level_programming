f6114b40879333ae2f7a9ec592b779c3a0324400
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-09-23 16:59:52 +0100

    Create 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *ht;
+    unsigned int i;
+
+    ht = malloc(sizeof(hash_table_t));
+
+    if (ht == NULL)
+    {
+        return 0;
+    }
+
+    ht->size = size;
+    ht->array = malloc(sizeof(hash_table_t *) * size);
+
+    for (i = 0; i < size; i++)
+    {
+        ht->array[i] = NULL; /* all spaces at each position
+        is null for now */
+    }
+
+    return ht;
+}

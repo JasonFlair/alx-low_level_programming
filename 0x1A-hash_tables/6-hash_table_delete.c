92a2e460492da691c95888736d6ad2450b492079
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-09-23 17:21:25 +0100

    Create 6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+   hash_table_t *head;
+  hash_node_t *node;
+  hash_node_t *temp;
+  unsigned long int i;
+  
+  head = ht;
+
+	for (i = 0; i < ht->size; i++)
+  {
+    if(h->array[i] != NULL)
+    {
+      node = h->array[i];
+    }
+    while (node != NULL)
+    {
+      temp = node->next;
+      free(node->value);
+      free(node->key);
+      node = temp;
+    }
+  }
+  free(ht->array);
+  free(head);
+  
+}

46462d92d0ca68d0fffd4f53ae97b02d20e520ee
JasonFlair <emelieobumse100@gmail.com>
2023-01-03 18:30:10 +0100

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,13 +1,21 @@
 #include "search_algos.h"
 #include <math.h>
 
+/**
+ * jump_search - search for a value in an array using the jump search algorithm
+ * @array: pointer to the first element of the array
+ * @size: number of elements in the array
+ * @value: value to search for
+ *
+ * Return: the index of the first occurrence of value in array, or -1 if value is not present
+ */
 int jump_search(int *array, size_t size, int value)
 {
     /*convert size from size_t to double*/
     double double_size = (double)size;
     size_t squareroot = sqrt(double_size);
     unsigned int start = 0;
-    unsigned end = squareroot;
+    unsigned int end = squareroot;
     unsigned int i;
 
     while (array[start] < value)

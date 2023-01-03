e4456bf6f2f38b69d4aba187fba905cca64e23de
JasonFlair <emelieobumse100@gmail.com>
2023-01-03 18:34:33 +0100

    fixed indentation

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,20 +7,23 @@
  *
  * Return: index of the first occurrence of value in the array, or -1 if not found
  */
-int linear_search(int *array, size_t size, int value) 
+int linear_search(int *array, size_t size, int value)
 {
     unsigned int i;
+
     if (array == NULL)
     {
         return -1;
     }
+
     for (i = 0; i < size; i++)
-      {
-	printf("Value checked array[%u] = [%d] \n", i, array[i]);
-	if (array[i] == value)
-	  {
-	    return i;
-	  }
-      }
+    {
+        printf("Value checked array[%u] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+        {
+            return i;
+        }
+    }
+
     return -1;
 }

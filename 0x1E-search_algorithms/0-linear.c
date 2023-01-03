c1d469f911c03856600918389cbcc2b834763235
Jason Flair <emelieobumse100@gmail.com>
2023-01-03 11:45:10 +0100

    fixed

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,18 +12,15 @@ int linear_search(int *array, size_t size, int value)
     unsigned int i;
     if (array == NULL)
     {
-        return (-1);
+        return -1;
     }
-    if (array)
-    {
-        for (i = 0; i < size; i++)
-        {
-            printf("Value checked array[%d] = [%d] \n", i, array[i]);
-            if (array[i] == value)
-            {
-                return i;
-            }
-        }
-    }
+    for (i = 0; i < size; i++)
+      {
+	printf("Value checked array[%d] = [%d] \n", i, array[i]);
+	if (array[i] == value)
+	  {
+	    return i;
+	  }
+      }
     return -1;
 }
